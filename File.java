68c991c9b20728b7be15d7d04437231599d2adbc
AMITKP07 <amitkparhi07@gmail.com>
2024-05-19 01:55:52 +0530

    add() added

--- File.java
+++ File.java
@@ -2,7 +2,16 @@ import java.lang.*;
 
 public class File1
 {
+
+	int x=1000;
+	int y=200;
+	public void add()
+	{
+		System.out.println(x+y);
 	public static void main(String[] args){
-	 System.out.println("First Java");
+		System.out.println("My 2nd line of code");
+		System.out.println("First Java");
+
 	}
+
 }
